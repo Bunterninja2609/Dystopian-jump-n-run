b964deecadd8deaf9d1ed23ba0fca44692399b2d
Bunterninja2609 <joshi2609@icloud.com>
2023-11-01 20:40:04 +0100

    heheheHA

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
--- Love2D Code Example with Physics and a Player Rectangle
+-- Love2D Code Example with Physics, Player Rectangle, and Ground
 
-local world, player
+local world, player, ground
 
 function love.load()
+
+    sti = require 'sti'
+    gameMap = sti('map/map.lua')
     love.window.setTitle("Dystopian game")
     love.window.setMode(800, 600, {resizable=true, vsync=false, minwidth=400, minheight=300})
 
@@ -10,40 +13,57 @@ function love.load()
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 9.81 * 64, true)
 
-    -- Create a rectangle
+    -- Create a rectangle for the player
     player = {}
     player.body = love.physics.newBody(world, 200, 300, "dynamic")
     player.shape = love.physics.newRectangleShape(50, 50)
     player.fixture = love.physics.newFixture(player.body, player.shape)
+
+    -- Create the ground
+    ground = {}
+    ground.body = love.physics.newBody(world, 400, 550, "static")
+    ground.shape = love.physics.newRectangleShape(800, 50)
+    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
 end
 
 function love.update(dt)
-    local contacs = player.body:getContactList()
+    local contacts = player.body:getContactList()
     local vx, vy = player.body:getLinearVelocity()
+
     -- Move the player left
     if love.keyboard.isDown('a') then
         player.body:setX(player.body:getX() - 2)
     end
-    if love.keyboard.isDown('space') and #contacs > 0 then
+
+    -- Jump if the player is on the ground
+    if love.keyboard.isDown('space') and #contacts > 0 then
         vy = vy - 10
     end
+
     -- Move the player right
     if love.keyboard.isDown('d') then
         player.body:setX(player.body:getX() + 2)
     end
+
     player.body:setLinearVelocity(vx, vy)
+    
     -- Update the physics world
     world:update(dt)
 end
 
 function love.draw()
     love.graphics.push()
-        love.graphics.translate(-player.body:getX() + love.graphics:getWidth()/2, 0)
-        -- Set the color to red
-        love.graphics.setColor(1, 0, 0)
+    love.graphics.translate(-player.body:getX() + love.graphics.getWidth()/2, 0)
+    gameMap:draw()
+    -- Set the color to red for the player
+    love.graphics.setColor(1, 0, 0)
+    -- Draw the player rectangle
+    love.graphics.polygon("fill", player.body:getWorldPoints(player.shape:getPoints()))
 
-        -- Draw the player rectangle
-        love.graphics.polygon("fill", player.body:getWorldPoints(player.shape:getPoints()))
+    -- Set the color to blue for the ground
+    love.graphics.setColor(0, 0, 1)
+    -- Draw the ground rectangle
+    love.graphics.polygon("fill", ground.body:getWorldPoints(ground.shape:getPoints()))
+
     love.graphics.pop()
 end
-
