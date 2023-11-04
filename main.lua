1be82b64cc89b9c392da823115b349c109200b4b
Bunterninja2609 <joshi2609@icloud.com>
2023-11-04 18:40:04 +0100

    hallo

--- main.lua
+++ main.lua
@@ -10,12 +10,12 @@ function love.load()
     love.window.setTitle("Dystopian game")
     love.window.setMode(800, 600, {resizable=true, vsync=false, minwidth=400, minheight=300})
     walls = {}
-    
+    mapPNG = love.graphics.newImage("map/map.png")
     -- Initialize the physics world
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 9.81 * 64, true)
 
-    background1 = love.graphics.newImage("background1.png")
+    background1 = love.graphics.newImage("background2.png")
 
     -- Create a rectangle for the player
     player = {}
@@ -35,9 +35,21 @@ function love.load()
         wall.fixture = love.physics.newFixture(wall.body, wall.shape)
         table.insert(walls, wall)
     end
+    for i, obj in pairs(gameMap.layers["Object Layer 1"].objects) do
+        local wall = {}
+        wall.x = obj.x + obj.width / 2 + 1600
+        wall.y = obj.y + obj.height / 2
+        wall.width = obj.width
+        wall.height = obj.height
+        wall.body = love.physics.newBody(world, wall.x, wall.y, "static")
+        wall.shape = love.physics.newRectangleShape(wall.width, wall.height)
+        wall.fixture = love.physics.newFixture(wall.body, wall.shape)
+        table.insert(walls, wall)
+    end
 end
 
 function love.update(dt)
+    
     local contacts = player.body:getContacts()
     local vx, vy = player.body:getLinearVelocity()
 
@@ -60,6 +72,16 @@ function love.update(dt)
     
     -- Update the physics world
     world:update(dt)
+    if player.body:getY() > 160 then
+        love.event.quit(0)
+    end
+    if player.body:getX() > 1600 then
+        player.body:setX(0)
+    end
+    if player.body:getX() < 0 then
+        player.body:setX(1600)
+    end
+
 end
 
 function love.draw()
@@ -67,23 +89,28 @@ function love.draw()
     local playerX, playerY = player.body:getX(), player.body:getY()
     local translateX = screenWidth / 2 / scale - playerX
     local translateY =  screenHeight / 2 / scale + screenHeight/2/scale - 160
-
+    love.graphics.setBackgroundColor(59/255, 55/255, 60/255)
     love.graphics.push()
     love.graphics.scale(scale)
     love.graphics.translate(translateX, translateY)
-    for i = 0, 10 do
+    for i = -10, 10 do
         love.graphics.setColor(0.5, 0.5, 0.5)
-        love.graphics.draw(background1, playerX/2 + i * background1:getWidth(),0)
+        love.graphics.draw(background1, playerX/2 + i * background1:getWidth(),-screenHeight/4)
     end
-    for i = 0, 10 do
+    for i = -10, 10 do
         love.graphics.setColor(0.7, 0.7, 0.7)
-        love.graphics.draw(background1, playerX/4 + i * background1:getWidth(),16)
+        love.graphics.draw(background1, playerX/5 + i * background1:getWidth(),-screenHeight/8)
         
     end
-    for i = 0, 10 do
+    for i = -10, 10 do
         love.graphics.setColor(1,1,1)
         
-        love.graphics.draw(background1, playerX/8 + i * background1:getWidth(),32 )
+        love.graphics.draw(background1, playerX/10 + i * background1:getWidth(),-screenHeight/32)
+    end
+    for i = -10, 10 do
+        love.graphics.setColor(1,1,1)
+        
+        love.graphics.draw(background1, playerX/20 + i * background1:getWidth(), 0)
     end
     
     love.graphics.setColor(1, 0, 0)
@@ -93,6 +120,8 @@ function love.draw()
     love.graphics.setColor(1, 1, 1)
 
     gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
+    love.graphics.draw(mapPNG, -1600, 0)
+    love.graphics.draw(mapPNG,  1600, 0)
     love.graphics.circle("fill",0,0,1)
     love.graphics.pop()
 end
